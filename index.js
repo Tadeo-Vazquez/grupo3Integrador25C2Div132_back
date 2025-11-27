1da6ca6fb853c25c97c67efe3f010b63ec597f49
emmanuelmartindavid <emmanuel.david.martin77@gmail.com>
2025-11-27 20:54:05 -0300

    feat: agregando login

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ import { loggerUrl } from "./src/api/middlewares/middlewares.js";
 import { rutasProductos, ventasProductos } from "./src/api/routes/index.js";
 import { __dirname, join } from "./src/api/utils/index.js";
 import connection from "./src/api/database/db.js";
-
+import session from "express-session";
 
 const app = express();
 
 const PORT = environments.port;
+
+const SESSION_KEY = environments.session_key;
 
 app.use(cors())
 
@@ -19,15 +21,18 @@ app.use(loggerUrl)
 
 app.use(express.json())
 
+app.use(express.urlencoded({extended: true}));
+
 app.use(express.static(join(__dirname,"src","public")))
 
-//configuracion
+
+
 
 app.set("view engine", "ejs")
 app.set("views", join(__dirname,"src","views"))
 
 
-app.get("/index", async (req,res)=>{
+app.get("/", async (req,res)=>{
     try{
         const [rows] = await connection.query("SELECT * FROM productos")
 
@@ -65,19 +70,86 @@ app.get("/modificar",(req,res)=>{
         })
 })
 
-
-
-
-
-
-// endpoints
-app.get("/", (req,res) => {
-    res.send("TP INTEGRADOR")
+app.get("/login",(req,res)=>{
+    res.render("login",{
+            title:"login",
+            about: "inciar sesion"
+        })
 })
+
+
+app.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+
+        if(!email || !password) {
+            return res.render("login", {
+                title: "Login",
+                about: "Login dashboard",
+                error: "Todos los campos son obligatorios"
+            });
+        }
+
+        const sql = "SELECT * FROM usuarios WHERE correo = ? AND password = ?";
+        const [rows] = await connection.query(sql, [email, password]);
+
+        if(rows.length === 0) {
+            return res.render("login", {
+                title: "Login",
+                about: "Login dashboard",
+                error: "Credenciales incorrectas"
+            })
+        }
+
+ 
+        const user = rows[0];
+        console.table(user);
+
+       
+        req.session.user = {
+            id: user.id,
+            nombre: user.nombre,
+            email: user.email
+        }
+
+        res.redirect("/"); 
+        
+
+    } catch (error) {
+        console.error("Error en el login", error);
+    }
+});
+
+
+app.post("/logout", (req, res) => {
+
+   
+    req.session.destroy((err) => {
+        if(err) { 
+            console.log("Error al destruir la sesion", err);
+            return res.status(500).json({
+                error: "Error al cerrar la sesion"
+            });
+        }
+
+       
+        res.redirect("/login");
+    });
+});
+
+
+
+app.use(session({
+    secret: SESSION_KEY,
+    resave: false,
+    saveUninitialized: true
+}));
+
+
 
 app.use("/api/productos", rutasProductos);
 app.use("/api", ventasProductos)
-
 
 
 
