b83388b2a91145d8612cbed541563b0c2426df86
Tadeo-Vazquez <tadeovazquez2006@gmail.com>
2025-12-04 21:22:24 -0300

    ultimos estilados, boton select

--- src/api/models/product.models.js
+++ src/api/models/product.models.js
@@ -49,6 +49,8 @@ export const selectProducts = async ({limit=10,offset=0,soloActivos=false,catego
 
     return { rows, total };
 };
+
+
 /**
  * Consulta para obtener un producto específico por su Id.
  *
@@ -59,6 +61,8 @@ export const selectProductById = (id) => {
     const sql = `SELECT * from productos WHERE productos.id = ?`;
     return connection.query(sql,[id]);
 }
+
+
 /**
  * Consulta para la creación de un nuevo producto en la DB.
  *
@@ -72,6 +76,8 @@ export const insertProduct = (nombre,categoria,precio,imagen) => {
     let consulta = "INSERT INTO productos(nombre, tipo, precio, img_url) VALUES (?,?,?,?)"
     return connection.query(consulta, [nombre,categoria,precio,imagen])
 }
+
+
 /**
  * Consulta para la actualización completa de un producto en la DB.
  *
@@ -87,6 +93,8 @@ export const updateProduct = (activo,nombre,categoria,precio,imagen,id) => {
     let consulta = "UPDATE productos SET activo=?,nombre=?,tipo=?,precio=?,img_url=? WHERE id = ?"
     return connection.query(consulta, [activo,nombre,categoria,precio,imagen,id])
 }
+
+
 /**
  * Consulta para eliminar un producto de la DB.
  *
@@ -97,6 +105,8 @@ export const deleteProduct = (id) => {
     let consulta = "delete from productos WHERE id = ?"
     return connection.query(consulta, [id])
 }
+
+
 /**
  * Consulta para actualizar solo el estado (activo/inactivo) de un producto.
  *
